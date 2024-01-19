ebb6c93e6e3f14e79abd21bb083cf836de63aeac
Leoka2000 <reusoliveiraleo@gmail.com>
2024-01-19 04:41:55 +0100

    second commit

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,11 +1,19 @@
-<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-end z-10">
+<div class="z-20 flex items-center justify-between w-full h-20 pr-2 border-b border-gray-700 sm:fixed sm:top-0 sm:right-0 text-end bg-slate-800">
+ <a href="{{ route('dashboard') }}" wire:navigate>
+                        <div class='flex items-center justify-center w-auto mt-2 w-36'>
+                            <img class='object-cover w-full h-full rounded-md' src="{{ asset('logo-top.png') }}"
+                                alt="sheesh" title="sheesh" />
+                        </div>
+                    </a>
     @auth
         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Dashboard</a>
     @else
-        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Log in</a>
+    <div>
+        <x-button primary href="{{ route('login') }}" class="font-semibold text-gray-300 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Log in</x-button>
 
         @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Register</a>
+            <x-button outline primary href="{{ route('register') }}" class="font-semibold text-gray-300 ms-4 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Register</x-button>
         @endif
+        </div>
     @endauth
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import aspectratio from '@tailwindcss/aspect-ratio';
+import typography from '@tailwindcss/typography';
+import colors from 'tailwindcss/colors.js';
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,13 +11,30 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+
+        './vendor/wireui/wireui/resources/**/*.blade.php',
+        './vendor/wireui/wireui/ts/**/*.ts',
+        './vendor/wireui/wireui/src/View/**/*.php'
     ],
+   
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            presets: [
+
+                require('./vendor/wireui/wireui/tailwind.config.js')
+        
+            ],
+            colors: {
+                primary: colors.violet,
+                background: {
+                    white: colors.white,
+                    dark: colors.slate[800]
+                  }
+            }
         },
     },
 
